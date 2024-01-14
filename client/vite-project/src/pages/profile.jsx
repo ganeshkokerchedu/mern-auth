5dbe9b11cdb1c5e3ca935c171e0931d7008cadb5
unknown <git.ganesh>
2024-01-14 20:23:08 +0530

    complete profile page ui

--- client/vite-project/src/pages/profile.jsx
+++ client/vite-project/src/pages/profile.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import {useSelector} from "react-redux";
 const profile = () => {
+  const {currentUser} = useSelector((state) => state.user)
   return (
-    <div>profile</div>
+    <div className="p-3 max-w-lg mx-auto">
+    <h1 className="text-3xl font-semibold text-center my-7">
+       Profile
+    </h1>
+    <form className="flex flex-col gap-4">
+      <img src={currentUser.profilePicture} alt="profile" className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2" />
+      <input defaultValue={currentUser.username} id="username" placeholder="Username" className="bg-slate-100 rounded-lg p-3" />
+      <input defaultValue={currentUser.email} id="username" placeholder="email" className="bg-slate-100 rounded-lg p-3" />
+      <input type="password" id="username" placeholder="password" className="bg-slate-100 rounded-lg p-3" />
+       <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">update</button>
+    </form>
+    <div className="flex justify-between mt-5">
+    <span className="text-red-700 curser-pointer">Delete Account</span>
+    </div>
+    
   )
 }
 
