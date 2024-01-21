33d1cc902a4a6b7177bfac5a1811566d03db5c51
unknown <git.ganesh>
2024-01-21 08:46:06 +0530

    add pre production changes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {test,updateUser, deleteUser , signout} from "../controllers/user.controller.js";
+import {test,updateUser, deleteUser } from "../controllers/user.controller.js";
 //import { verifyToken } from "../utils/varifyUser.js";
 const router = express.Router();
 
 router.get("/", test);
 router.post("/update/:id",  updateUser);
 router.delete("/delete/:id", deleteUser);
-router.get("/signout", signout);
+
 export default router;
