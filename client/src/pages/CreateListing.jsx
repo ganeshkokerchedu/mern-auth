bc2723a5ef9c2078269fd9ad677ae6b5e2754c07
unknown <git.ganesh>
2024-01-26 19:26:38 +0530

    complete create listing page ui

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ export default function CreateListing() {
       return (
             <main className="p-3 max-w-4xl mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Create a Listing</h1>
-            <form className="flex flex-col sm:flex-row ">
+            <form className="flex flex-col sm:flex-row gap-4">
             <div className="flex flex-col gap-4 flex-1">
             <input type="text" placeholder="Name" className="border p-3 rounded-lg" id="name" maxLength="62" minLength="10" required />
             <textarea type="text" placeholder="Description" className="border p-3 rounded-lg" id="description"  />
@@ -50,10 +50,11 @@ export default function CreateListing() {
             <input type="number" id="regularPrice" min="1" max="10" required className="p-3 border border-gray-300 rounded-lg"/>
             <p>
             Regular price
-            <span className="text-xs">
-            {$ / month}
-            </span>
+            
             </p>
+            <span className="text-xs">
+            {`$ / month`}
+            </span>
             
             </div>
             </div>
@@ -63,13 +64,24 @@ export default function CreateListing() {
            Discounted price
             </p>
             <span className="text-xs ">
-            {$ / month}
+            {`$ / month`}
             </span>
             
             </div>
             </div>
             </div>
-           
+            <div className="flex flex-col flex-1 gap-4">
+             <p className="font-semibold">
+             Images:
+             <span className="font-normal text-gray-600 ml-2">The first image will be the cover (max-6) </span>
+             </p>
+             <div className="">
+              <input className="p-3 border border-gray-300 rounded w-full" type="file" id="images" accept="images/*" multiple />
+              <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80" >Upload</button>
+             </div>
+             <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Create Listing</button>
+
+            </div>
             
             </form>
             </main>
